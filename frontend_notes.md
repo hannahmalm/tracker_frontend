1c585d11e17c9dee88274440bde29b87324c2dbe
Hannah Malm <hannahemalm616@gmail.com>
2022-01-13 08:37:24 -0500

    updated dom content loaded

--- frontend_notes.md
+++ frontend_notes.md
@@ -1,3 +1,5 @@
 Notes
 
 1. The first event in JS is a listener that the DOM content loads - get fetch request - render the data 
+2. find the endpoint and set it to a const
+3. 
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-//Console log once the DOM is loaded - get a fecth request to backend rails 
+const exercsieURL = "http://localhost:3000/api/v1/exercises"
+
+//Console log once the DOM is loaded - get a fecth request to backend rails index method
+//The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
 document.addEventListener('DOMContentLoaded'), () => {
-    console.log('loaded'); 
-}
+    fetch("exerciseURL") //fetch return returns a promise, in the fetch request return to json 
+    .then(response => response.json) //parse response into json
+    .then(exercise => {
+        console.log(exercise);
+    })//get access to json data. exercise is an array of exercises
