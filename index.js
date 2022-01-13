9be3e94ec70678d1cd38134f2fcf45880a4a72e9
Hannah Malm <hannahemalm616@gmail.com>
2022-01-13 15:07:23 -0500

    queried form values in form handler

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const exerciseURL = "http://localhost:3000/api/v1/exercises"
 //Console log once the DOM is loaded - get a fecth request to backend rails index method
 //The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
 document.addEventListener('DOMContentLoaded', () => {
-    getExercise()
+    getExercise() //render the exercise arrays
+
+    const createExerciseForm = document.querySelector("#create-new-exercise-form") //query the exercise form (in html)
+
+    //After querying the form, the user will input data
+    //Wait and listen for the user to submit the form
+    createExerciseForm.addEventListener("submit", (e) => createFormHandler(e))
+    //prevent the page from auto refreshing - create a handler to do this
+    //This handler will prevent this from auto refreshing, and will post the form 
+
 })
 
 
@@ -29,5 +38,20 @@ function getExercise() {
         //render the exercises
     })
 } //navigate to the index.js and view xhl to ensure this works 
+
+
+function createFormHandler(e){ //handle the form inputs, prevent the default, and do something with it
+    //prevent the default behavior 
+    e.preventDefault()
+    debugger  //get values of inputs - query for inputs and get the value
+    //document.querySelector("#class-name").value
+    //exercise_name = document.querySelector('#exercise-name').value
+    //exercise_instructions = document.querySelector('#exercise-name').value
+    //exercise_image = document.querySelector('#exercise-image').value
+    //category = document.querySelector('#category').value
+  
+   
+    //find input values by using the debugger
+}
 
 
