6f525d416f7c4f1acfd72fd4b533f53df049947e
Hannah Malm <hannahemalm616@gmail.com>
2022-01-13 08:21:50 -0500

    added notes and dom content loaded

--- frontend_notes.md
+++ frontend_notes.md
@@ -1,1 +1,3 @@
 Notes
+
+1. The first event in JS is a listener that the DOM content loads - get fetch request - render the data 
---
+++ index.js
@@ -0,0 +1,4 @@
+//Console log once the DOM is loaded - get a fecth request to backend rails 
+document.addEventListener('DOMContentLoaded'), () => {
+    console.log('loaded'); 
+}
