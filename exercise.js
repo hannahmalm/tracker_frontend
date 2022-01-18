cf9ab76339f175c120b20d384dd73453eac309f5
Hannah Malm <hannahemalm616@gmail.com>
2022-01-18 16:46:52 -0600

    added in constructor and blank array

--- exercise.js
+++ exercise.js
@@ -6,6 +6,32 @@
 //You need a place to store ids in the HTML elements to determine which exercise gets clicked and show more information about the exercise
 // ENSURE YOU ARE PUTTING data-ids on all divs when rendering 
 //Why push it in the array? You do an initial get fetch to get all exercises, so put them in an array and store them
+//https://www.youtube.com/watch?v=HboT8g_QSGc
 
 
+class Exercise {
 
+
+    //function that constructs/creats a new object (exercise object) -> You could call var newExercise = new Exercise();
+    //pass the constructor the arguments of what you want each Exercise to have
+    constructor(exercise, exercisesAttributes) {
+        this.id = exercise.id 
+        this.name = exercisesAttributes.name;
+        this.instructions = exercisesAttributes.instructions;
+        this.image = exercisesAttributes.image;
+        this.category_id = exercisesAttributes.category_id;
+        Exercise.call.push(this)
+
+    }
+}
+
+var squat = new Exercise("squat", "Instructions for squat", "image", "3")
+
+console.log(squat); 
+
+//the new Keyword - You could call var newExercise = new Exercise(HammerCurl, Instructions for it, image for it);
+    //creates a new empty object
+    // sets the value of "this" to be a new empty object
+    // calls the constructor method
+    //this = the new object that was created
+    //you can call properities on this; like this.email 
