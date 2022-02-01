e0552a4f863686afd8191fccd1ce7ccc6e61139c
Hannah Malm <hannahemalm616@gmail.com>
2022-01-31 18:14:37 -0600

    deleted out unused css from bootstrap;

--- index.js
+++ index.js
@@ -1,67 +1,47 @@
 const exerciseURL = "http://localhost:3000/api/v1/exercises"
-
-// const cardList = document.querySelector('.card-group');
-
-// const cardBody = document.querySelector('.card');
-
 //--------------Variables for Editing form------------------------------------------------
 const nameValue = document.getElementById('exercise-name'); //taken from index.html within the form 
 const bodyValue = document.getElementById('exercise-instructions'); //taken from index.html within the form
 const videoValue = document.getElementById('exercise-image'); //taken from index.html within the form
-const categoryValue = document.getElementById('categories').selectedIndex; //taken from index.html within the form
-//const categoryValue = document.querySelector('categories select option:checked');
+//const categoryDropdown = document.getElementById('categories') //taken from index.html within the form
+//const categorySelectedIndex = categoryDropdown.options[categoryDropdown.selectedIndex];
 const btnSubmit = document.querySelector('#create-exercise');
-//-----------------------------------------------------------------------------------------
 
-//Console log once the DOM is loaded - get a fecth request to backend rails index method
-//The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading
-//The load event fires when a resource and all its dependent resources (including CSS and JavaScript) have finished loading
+
+//--------------Variables for Creating form------------------------------------------------
+const createExerciseForm = document.querySelector("#create-new-exercise-form") //query the exercise form (in html)
+//-----------------------------------------------------------------------------------------
 //The DOMContentLoaded event is the browser's built-in way to indicate when a page's html is loaded into the DOM
 //It isn't possible to manipulate HTML elements that haven't rendered yet, so trying to manipulate the DOM before the page fully loads can potentially lead to problems.
-//you need to wait until the dom content is all loaded to render the form and the exercise cards
-//By creating an event listener, you keep the code from immediately firing when index.js is loaded
 //document.addEventListener('DOMContentLoaded', () => {     //This is how you write it in es6
-//It is important to note that the DOMContentLoaded event fires once the initial HTML document finishes loading, but does not wait for CSS stylesheets or images to load.
-//It may take a couple of seconds for the youtube videos to load
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("dom loaded")
+document.addEventListener("DOMContentLoaded", function() { //By creating an event listener, you keep the code from immediately firing when index.js is loaded
+    console.log("dom loaded") //ensure dom is loadedd
     getExercise() //render and fetch all the exercise cards within the exercise array 
-    const createExerciseForm = document.querySelector("#create-new-exercise-form") //query the exercise form (in html)
-
+    createExerciseForm.addEventListener("submit", (e) => createFormHandler(e))
     //After querying the form, the user will input data
     //Wait and listen for the user to submit the form
-    createExerciseForm.addEventListener("submit", (e) => createFormHandler(e))
     //prevent the page from auto refreshing - create a handler to do this
-    //This handler will prevent this from auto refreshing, and will post the form 
-
+   
     document.addEventListener("dblclick", (e) => createClickHandler(e))
    
-    
-
 })
-
-//ADD IN AN ALERT TO USER WHEN FORM IS SUBMITTED
-// const input = document.getElementById('input');
-// input.addEventListener('click', function(event) {
-//   alert('I was clicked!');
-// });
 
 //create a function to get the array of all exercises
 //fetch() is similar to XMLHttpRequest that has promises
 //fetch the url bakend
 //using .them allows you to chain multiple asynchronous operations to run in order, one after the other 
 //fetch by default users an HTTP GET to retreive content from the URL
+//fetch requests to the index 
 function getExercise() {
     fetch(exerciseURL) //fetch return returns a promise, in the fetch request return to json 
     .then(response => response.json()) //parse response into json -> this is a callback function that will run if the previous operation is successful
     .then(exercise => { //get the exercise ARRAY - since its an array, you need to iterate -> This callback receives the input of the previous callback
         exercise.data.forEach(exercises => {
-            // debugger; //Use debugger to type in exercise.attributes to find all the attributes
             //ensure you have a data-id to work with OOJS
             //Data Ids let you know which exercise you are making/clicking on
             //Without data id youd have to do: @exercise = Exercise.find_by(name: params[:name])
             //create a new instance of the Exercise class for every exercise in the array
-            let newExercise = new Exercise(exercises, exercises.attributes)
+            let newExercise = new Exercise(exercises, exercises.attributes) //Correlates with class Exercise
             
             // const exerciseHTML = `
             // <div data-id=${exercises.id}> 
@@ -75,9 +55,7 @@ function getExercise() {
             //add the new HTML into the div container on html to render it 
             //document.querySelector('#exercise-container').innerHTML += exerciseHTML
             document.querySelector('#exercise-container').innerHTML += newExercise.renderExerciseCard()
-
-        })//get access to json data. exercise is an array of exercises
-        //render the exercises
+        })
     })
 } //navigate to the index.js and view xhl to ensure this works 
 
@@ -88,7 +66,8 @@ function createClickHandler(e){
     let editButton = e.target.id == 'edit-exercise';
     //console.log(e.target.parentElement.parentElement.parentElement.parentElement.dataset.id)
     let id = e.target.parentElement.parentElement.parentElement.parentElement.dataset.id;
-
+    //Delete the existing post using DELETE method
+    //Add in destroy controller method and route
     if(deleteButton){
         console.log("delete");
         fetch((`${exerciseURL}/${id}`), { method: 'DELETE' })
@@ -96,35 +75,31 @@ function createClickHandler(e){
         .then(() => location.reload())
         
     }
-    
+        
     if(editButton){
         console.log("edit")
         const parent = e.target.parentElement.parentElement.parentElement;
-        let nameContent = parent.querySelector('.card-title').textContent;
-        let instructionsContent = parent.querySelector('.card-text').textContent; 
+        const nameContent = parent.querySelector('.card-title').textContent;
+        const instructionsContent = parent.querySelector('.card-text').textContent; 
         const videoParent = e.target.parentElement.parentElement.parentElement.parentElement;
         const categoryParent = e.target.parentElement.parentElement;
-        //console.log(categoryParent.querySelector('.text-muted').textContent)
         //console.log(videoParent.querySelector('#myframe').attributes.src.value)
-        let videoContent = videoParent.querySelector('#myframe').attributes.src.value;
-        //let categoryContent = categoryParent.querySelector('.text-muted').textContent;
+        const videoContent = videoParent.querySelector('#myframe').attributes.src.value;
+        //const categoryContent = categoryParent.querySelector('.text-muted').textContent;
+        //console.log(categoryParent.querySelector('.text-muted').textContent)
+        // console.log(categoryContent)
+        // console.log(categorySelectedIndex)
         // console.log(videoContent)
         // console.log(nameContent);
         // console.log(instructionsContent);
-        // console.log(videoContent);
-        //console.log(categoryValue)
         nameValue.value = nameContent; //renders old content in the form
         bodyValue.value = instructionsContent; //renders old content in the form
         videoValue.value = videoContent; //renders old content in the form
-        // console.log(categoryValue.value)
-        // console.log(categoryValue)
-        // console.log(categoryContent)
-        //categoryValue.value = categoryContent; //renders old content in the form
-
+        //categorySelectedIndex = categoryContent; //renders old content in the form
     }
     
     //Update the existing post using PATCH method
-    //Ensure that this is done on the backend within the controller and is in the routes
+    //Add in Update controller method and route
     btnSubmit.addEventListener('click',(e) => {
         e.preventDefault()
         console.log('updated')
@@ -136,8 +111,7 @@ function createClickHandler(e){
             body: JSON.stringify({
                 name: nameValue.value,
                 instructions: bodyValue.value,
-                image: videoValue.value,
-                //category_id: categoryValue.value
+                image: videoValue.value
             })
         })
             .then(() => console.log('edit success'))
@@ -149,9 +123,9 @@ function createClickHandler(e){
 
 
 
+ //This handler will prevent this from auto refreshing, and will post the form 
 function createFormHandler(e){ //handle the form inputs, prevent the default, and do something with it
-    //prevent the default behavior from refreshing the page
-    e.preventDefault()
+    e.preventDefault() //prevent the default behavior from refreshing the page
     console.log("submitted")
     //alert('Your exercise was submitted. To view this exercise, scroll to the bottom of this page');
     // debugger  //get values of inputs - query for inputs and get the value
@@ -187,18 +161,13 @@ function postFetch(name, instructions, image, category_id) {
     })
     .then(response => response.json()) //handle responses to fetch() -> server will send a response, to access this response use a .then() callback
     //This response object has a built in .json method, converting the body of the response from JSON to a JS object
-    .then(() => location.reload())
+    .then(() => location.reload()) //clear the form 
     .then(exercise => { //in this callback, the js object from the response can be used
         console.log(exercise); //always console log so that we can see what the code looks like 
         const exerciseData = exercise.data
         let newExercise = new Exercise(exerciseData, exerciseData.attributes)
         document.querySelector('#exercise-container').innerHTML += newExercise.renderExerciseCard()
     
-
-    
-
-
-
         // const exerciseMarkup = `
         // <div data-id=${exercise.id}> 
         //     <h2>${exerciseData.attributes.name}</h2>
@@ -212,16 +181,4 @@ function postFetch(name, instructions, image, category_id) {
     //     alert("Oops, something went wrong");
         
     }) //catch is called when something goes wrong that allows us to handle th error
-        
-
-  
-  //reset input fields to empty the form 
-
-
-
-   
-
-   
-
-    
 }
